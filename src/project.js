428dcc2f71cdb980cda52a4be698f21eb6ffcbaa
Abhishek M. <abhishek.m@cloudium.io>
2024-04-19 16:44:38 +0530

    first commit

---
+++ src/project.js
@@ -0,0 +1,23 @@
+class project {
+
+    constructor(name) {
+        this.name = name;
+        this.todos = []
+    }
+
+    addtodo(todo) {
+        this.todos.push(todo);
+
+    }
+    removetodo(todo) {
+        this.todos = this.todos.filter(t => t !== todo);
+    }
+}
+
+
+export default project;
+
+
+
+
+
